50be585de1fe0e9ea6319b92c2e2db381abf5d9b
root <root@hk-front-web.asia-east2-a.c.x-micron-314706.internal>
2025-09-25 09:34:48 +0800

    first commit

---
+++ cn.js
@@ -0,0 +1,53 @@
+window.cn = {
+
+    // "title": "日日生",
+
+    // 九宮格遊戲名稱
+    // "indexGame1": "越南彩票",
+    // "indexGame2": "老虎机",
+    // "indexGame3": "龙城棋牌",
+    // "indexGame4": "捕鱼",
+    // "indexGame5": "皇冠体育",
+    // "indexGame6": "21点",
+    // "indexGame7": "番摊",
+    // "indexGame8": "骰宝",
+    // "indexGame9": "鱼虾蟹"
+
+    // 改各遊戲錢包顯示名稱
+    // "VRGame_wallet": "VR彩票",
+    // "818Game_wallet": "818娱乐",
+    // "BLinkGame_wallet": "KC皇冠体育",
+    // "RTGGame_wallet": "KC电子",
+    // "BGGame_wallet": "KC彩票 KC真人",
+    // "BLGame_wallet": "KC棋牌",
+    // "DTGame_wallet": "DT电子",
+    // "NGGGame_wallet": "GG彩票",
+    // "WMGame_wallet": "WM真人",
+    // "LCGame_wallet": "龙城棋牌",
+    // "IconicGame_wallet": "IG电子 IG捕魚"
+    // "YuanbaoGame_wallet": "越南彩票",
+
+
+
+    // 遊戲平台名稱
+    // "VRGame": "VR彩票",
+    // "818Game": "818娱乐",
+    // "BLinkGame": "KC皇冠体育",
+    // "RTGGame": "RTG电子",
+    // "BGGame": "KC真人",
+    // "BLGame": "KC棋牌",
+    // "DTGame": "DT电子",
+    // "NGGGame": "GG彩票",
+    // "WMGame": "WM真人",
+    // "LCGame": "龙城棋牌",
+    // "IconicGame": "Iconic Game",
+    // "ZYGame": "KC棋牌",
+    //"DreamGame": "DG真人"
+
+    // 幣別代號
+    "currency_value": "RMB",
+
+    //copyright
+    "copyright":"版权声明 : Copyright © 娱乐版权所有 Reserved"
+
+};
---
+++ vi.js
@@ -0,0 +1,29 @@
+window.vi = {
+
+    "title": "Cửu Long",
+
+    // 九宮格遊戲名稱
+    // "indexGame1": "Xổ Số Việt Nam",
+    // "indexGame2": "Máy đanh bạc",
+    // "indexGame3": "Bàn trò chơi",
+    // "indexGame4": "Máy câu cá",
+    // "indexGame5": "Xổ số thể thao",
+    // "indexGame6": "Black Jack",
+    // "indexGame7": "Fan Tan",
+    // "indexGame8": "Sicbo Trăm Người",
+    // "indexGame9": "Tôm Cua Cá"
+
+    // 改各遊戲錢包顯示名稱
+    // "VRGame_wallet": "VR彩票",
+    // "818Game_wallet": "818娱乐",
+    // "BLinkGame_wallet": "KC皇冠体育",
+    // "RTGGame_wallet": "KC电子",
+    // "BGGame_wallet": "KC彩票 KC真人",
+    // "BLGame_wallet": "KC棋牌",
+    // "DTGame_wallet": "DT电子",
+    // "NGGGame_wallet": "GG彩票",
+    // "WMGame_wallet": "WM真人",
+    // "LCGame_wallet": "龙城棋牌",
+    // "IconicGame_wallet": "IG电子 IG捕魚"
+    // "YuanbaoGame_wallet": "越南彩票"
+};
